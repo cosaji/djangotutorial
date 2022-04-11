ef4624890b0ddfe9cfbc9e038f61a9716637fae8
Mariko <hitosaji53@gmail.com>
2022-04-11 19:03:49 +0900

    フォームの実装

---
+++ mydiaryproject/diary/forms.py
@@ -0,0 +1,8 @@
+from dataclasses import fields
+from django import forms
+from .models import Diary
+
+class DiaryForm(forms.ModelForm):
+	class Meta:
+		model = Diary
+		fields = ('date', 'title', 'text',)
